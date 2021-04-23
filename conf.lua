347de9f66446ce91a2bb6e3c732b527070b56d0e
Omar Mozo <78625581+andreyuscamayta@users.noreply.github.com>
2021-04-23 00:42:21 -0300

    Add files via upload
    initial

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "Disparando"
+	--t.version = "0.1"       
+	t.window.width = 480    --1080
+	t.window.height = 800	--1920
+
+	-- For Windows debugging
+	t.console = true
+end
